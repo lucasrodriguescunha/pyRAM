3dd7bc746a186ece1f324c99b06826887ed9cb01
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-07-27 11:27:47 -0300

    refactor: memory info output format for better readability

--- src/main.py
+++ src/main.py
@@ -9,10 +9,10 @@ def info_memory():
     available_gb = memory.available / (1024 ** 3)
     percent_used = memory.percent
 
-    print(f'Memória total: ${total_gb} GB')
-    print(f'Memória usada: ${used_gb} GB')
-    print(f'Memória disponível: ${available_gb} GB')
-    print(f'Porcentagem em uso: ${percent_used}%')
+    print(f'Memória total: {total_gb:.1f} GB')
+    print(f'Memória usada: {used_gb:.1f} GB')
+    print(f'Memória disponível: {available_gb:.1f} GB')
+    print(f'Porcentagem em uso: {percent_used:.1f}%')
 
 
 if __name__ == "__main__":
